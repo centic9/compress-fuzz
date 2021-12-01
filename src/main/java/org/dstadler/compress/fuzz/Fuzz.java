16340513fb0d6ec2514aabdc4bdafbd288fa82fb
Dominik Stadler <dominik.stadler@gmx.at>
2021-12-01 12:10:02 +0100

    Add decompression to the fuzzing

--- src/main/java/org/dstadler/compress/fuzz/Fuzz.java
+++ src/main/java/org/dstadler/compress/fuzz/Fuzz.java
@@ -7,6 +7,9 @@ import org.apache.commons.compress.archivers.ArchiveEntry;
 import org.apache.commons.compress.archivers.ArchiveException;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
 import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.compressors.CompressorException;
+import org.apache.commons.compress.compressors.CompressorInputStream;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
 
 /**
  * This class provides a simple target for fuzzing Apache Commons Compress with Jazzer.
@@ -29,10 +32,32 @@ public class Fuzz {
 				}
 			}
 		} catch (ArchiveException | IOException |
-				// many different runtime-exceptions are
+				// many runtime-exceptions are
 				// thrown with corrupt files
 				RuntimeException e) {
 			// expected here
 		}
+
+		try {
+			CompressorInputStream input = new CompressorStreamFactory(false,
+						// enable safety feature which limits how much memory can be allocated
+						1024)
+					.createCompressorInputStream(new ByteArrayInputStream(inputData));
+
+			// read the input stream
+			byte[] bytes = new byte[1024];
+			while (true) {
+				int read = input.read(bytes);
+				if (read < 0) {
+					break;
+				}
+			}
+		} catch (CompressorException | IOException |
+				// many runtime-exceptions are
+				// thrown with corrupt files
+				RuntimeException e) {
+			// expected here
+		}
+
 	}
 }
