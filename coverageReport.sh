64073511b8fef11ff743a5a6ad49138e4e9d884d
Dominik Stadler <dominik.stadler@gmx.at>
2022-01-12 11:38:25 +0100

    Adjust coverage-script to run Jazzer with all files from the corpus

--- coverageReport.sh
+++ coverageReport.sh
@@ -10,16 +10,8 @@
 set -eu
 
 
-# Remove any previous execution and make sure testing is triggered fully
-./gradlew clean
-
-
-# Execute the test with JaCoCo enabled
-./gradlew check
-
-
-# Fetch jar-dependencies
-./gradlew getDeps
+# Build the fuzzer and fetch dependency-jars
+./gradlew shadowJar getDeps
 
 
 # extract jar-files of Apache Commons Compress
@@ -35,10 +27,31 @@ done
 cd -
 
 
+# Fetch JaCoCo Agent
+wget --continue https://repo1.maven.org/maven2/org/jacoco/jacoco/0.8.7/jacoco-0.8.7.zip
+unzip -o jacoco-0.8.7.zip lib/jacocoagent.jar
+mv lib/jacocoagent.jar build/
+rmdir lib
+
+mkdir -p build/jacoco
+
+
+# Run Jazzer with JaCoCo-Agent to produce coverage information
+./jazzer \
+  --cp=build/libs/compress-fuzz-all.jar \
+  --instrumentation_includes=org.apache.commons.** \
+  --target_class=org.dstadler.compress.fuzz.Fuzz \
+  --nohooks \
+  --jvm_args="-javaagent\\:build/jacocoagent.jar=destfile=build/jacoco/corpus.exec" \
+  -rss_limit_mb=4096 \
+  -runs=0 \
+  corpus
+
+
 # Finally create the JaCoCo report
-java -jar /opt/poi/lib/util/jacococli.jar report build/jacoco/test.exec \
+java -jar /opt/poi/lib/util/jacococli.jar report build/jacoco/corpus.exec \
  --classfiles build/compressfiles \
- --sourcefiles /opt/apache/commons-compress/dist/source \
+ --sourcefiles /opt/apache/commons-compress/git/src/main/java/ \
  --html build/reports/jacoco
 
 
